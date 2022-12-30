87c50a9c4d7b556db6a5d3edc4bc043c699526b7
Bartosz Ciereszyński <80280@student.pb.edu.pl>
2022-12-30 01:03:46 +0100

    Forbbiden words crud adjustment

--- AdvertProject/Controllers/ForbidenWordsController.cs
+++ AdvertProject/Controllers/ForbidenWordsController.cs
@@ -21,20 +21,7 @@ namespace AdvertProject.Controllers
             return View(db.ForbidenWords.ToList());
         }
 
-        // GET: ForbidenWords/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ForbidenWord forbidenWord = db.ForbidenWords.Find(id);
-            if (forbidenWord == null)
-            {
-                return HttpNotFound();
-            }
-            return View(forbidenWord);
-        }
+   
 
         // GET: ForbidenWords/Create
         public ActionResult Create()
@@ -59,36 +46,6 @@ namespace AdvertProject.Controllers
             return View(forbidenWord);
         }
 
-        // GET: ForbidenWords/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ForbidenWord forbidenWord = db.ForbidenWords.Find(id);
-            if (forbidenWord == null)
-            {
-                return HttpNotFound();
-            }
-            return View(forbidenWord);
-        }
-
-        // POST: ForbidenWords/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Content")] ForbidenWord forbidenWord)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(forbidenWord).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(forbidenWord);
-        }
 
         // GET: ForbidenWords/Delete/5
         public ActionResult Delete(int? id)
